5a3bcac4a58967d19a955f45d59091d4a06510c1
Danny P <danny.pfeiffer@pantheon.io>
2023-03-11 21:07:31 -0700

    try a multisite, take 1

---
+++ web/sites/ms1.dev-dp-d91.pantheonsite.io/settings.php
@@ -0,0 +1,44 @@
+<?php
+#setting site name variable for local use
+#creating a new multisite instance requires you to change this variable
+#Be sure to change this to the same identifier that
+#you use under ‘services’ in your .lando.yml file.
+$site_name = "ms1.dev-dp-d91.pantheonsite.io";
+
+/**
+ * Load services definition file.
+ */
+$settings['container_yamls'][] = __DIR__ . '/services.yml';
+
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all environments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to ensure that
+ *      the site settings remain consistent.
+ */
+include "../default/settings.pantheon.php";
+
+/**
+ * Skipping permissions hardening will make scaffolding
+ * work better, but will also raise a warning when you
+ * install Drupal.
+ *
+ * https://www.drupal.org/project/drupal/issues/3091285
+ */
+// $settings['skip_permissions_hardening'] = TRUE;
+
+$databases['default']['default'] = array (
+  'database' => 'pantheon',
+  'username' => 'pantheon',
+  'password' => 'UHvFKFoJqxYirw9zy3Pj3AUUHMzDas15',
+  'prefix' => '',
+  'host' => 'dbserver.dev.a8efb50e-a307-45b2-a204-10f9855867c9.drush.in',
+  'port' => '16501',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['hash_salt'] = 'wzuUwbK7eKmLG335iAOUT6cvDJ23Gzd1w_DjPpk-aF76xCBi7mK8GLQ2oaLDX3XxTcXL6IPwrw';
