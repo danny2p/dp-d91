d4ecb756b2bc2588a1c7db0656dfbd95172ed68f
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2023-06-15 18:54:11 -0600

    perms

---
+++ web/private/scripts/qg_tunnel.php
@@ -0,0 +1,7 @@
+<?php
+putenv("QUOTAGUARDSTATIC_URL=https://o7b7e0w6ktj2rd:evflea1kz6t363bg8pw288jxme@us-east-shield-04.quotaguard.com:9294");
+print "Starting qgtunnel \n";
+shell_exec("../../../bin/qgtunnel &>/dev/null &");
+shell_exec("disown");
+print "Started qgtunnel";
+?>
