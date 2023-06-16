9e96ce780e9950c1cb4484cb67e9c9ddfdfe6261
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2023-06-15 22:29:52 -0600

    stuff

--- web/qg-db.php
+++ web/qg-db.php
@@ -1,12 +1,6 @@
 <?php
-putenv("QUOTAGUARDSTATIC_URL=https://o7b7e0w6ktj2rd:evflea1kz6t363bg8pw288jxme@us-east-shield-04.quotaguard.com:9294");
-print "starting qgtunnel \n";
-passthru("sh test.sh");
-passthru("disown");
-print "qgtunnel started";
-
 $host = '127.0.0.1';
-$port = 3309;
+$port = 3307;
 $database = 'drupal';
 $username = 'pantheon';
 $password = 'pantheon';
