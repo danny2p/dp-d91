3aa3ef52f7687cc2c71916cdbc2a5896637d64e4
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2023-06-15 21:12:12 -0600

    connect directly

--- web/qg-db.php
+++ web/qg-db.php
@@ -1,4 +1,9 @@
 <?php
+putenv("QUOTAGUARDSTATIC_URL=https://o7b7e0w6ktj2rd:evflea1kz6t363bg8pw288jxme@us-east-shield-04.quotaguard.com:9294");
+print "starting qgtunnel \n";
+passthru("sh test.sh");
+passthru("disown");
+print "qgtunnel started";
 
 $host = '127.0.0.1';
 $port = 3309;
