787a6ace7f3f68cafa2279859c496cedfa52d3bf
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2023-06-15 21:05:08 -0600

    trying other stuff

--- web/start_qgtunnel.php
+++ web/start_qgtunnel.php
@@ -1,7 +1,7 @@
 <?php
 putenv("QUOTAGUARDSTATIC_URL=https://o7b7e0w6ktj2rd:evflea1kz6t363bg8pw288jxme@us-east-shield-04.quotaguard.com:9294");
 print "starting qgtunnel \n";
-shell_exec("../bin/qgtunnel &>/dev/null &");
-shell_exec("disown");
+passthru("sh test.sh");
+passthru("disown");
 print "qgtunnel started";
 ?>
