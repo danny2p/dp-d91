80fe922b8ac64d5d66d4a2355af9f6e551c103d8
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2023-06-15 20:30:49 -0600

    correct path

--- web/start_qgtunnel.php
+++ web/start_qgtunnel.php
@@ -1,6 +1,6 @@
 <?php
 putenv("QUOTAGUARDSTATIC_URL=https://o7b7e0w6ktj2rd:evflea1kz6t363bg8pw288jxme@us-east-shield-04.quotaguard.com:9294");
 print "Starting qgtunnel \n";
-echo shell_exec("../../../bin/qgtunnel");
+echo shell_exec("../bin/qgtunnel");
 print "Started qgtunnel";
 ?>
