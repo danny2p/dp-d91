43c0009f16b990ca428c6fc3ec06db64eb196130
Danny P <danny.pfeiffer@pantheon.io>
2023-06-22 19:07:59 -0600

    print appserver binding

--- web/bindings.php
+++ web/bindings.php
@@ -2,16 +2,13 @@
 header('Content-Type: application/json; charset=utf-8');
 
 /**
- * Retrieve DB Credentials
+ * Get binding data
  */
-function getDatabaseCredentials()
-{
-  if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-    if (function_exists('pantheon_curl')) {
-      $url = 'https://api.live.getpantheon.com/sites/self/bindings?type=appserver';
-      $req = pantheon_curl($url, NULL, 8443);
-      $bindings = json_decode($req['body'], TRUE);
-      print $req['body'];
-    }
+if (isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+  if (function_exists('pantheon_curl')) {
+    $url = 'https://api.live.getpantheon.com/sites/self/bindings?type=appserver';
+    $req = pantheon_curl($url, NULL, 8443);
+    $bindings = json_decode($req['body'], TRUE);
+    print $req['body'];
   }
 }
