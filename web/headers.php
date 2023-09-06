de93d2ef019c6d87bd084026a641f299838ee065
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2023-09-06 09:29:33 -0600

    add http_host to headers.php

--- web/headers.php
+++ web/headers.php
@@ -1,4 +1,10 @@
 <table>
+    <tr>
+        <td style='width:200px;'>HTTP_HOST</td><td><?php echo $_SERVER['HTTP_HOST']; ?></td>
+    </tr>
+    <tr>
+        <td style='width:200px;'>SERVER_NAME</td><td><?php echo $_SERVER['SERVER_NAME']; ?></td>
+    </tr>
 <?php
 
 foreach (getallheaders() as $name => $value) {
