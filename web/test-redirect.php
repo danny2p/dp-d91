f6284cf183ac6f97f1c346bdceded249db555345
Danny Pfeiffer <danny.pfeiffer@pantheon.io>
2023-09-06 10:48:17 -0600

    removing extraneous else statement

--- web/test-redirect.php
+++ web/test-redirect.php
@@ -4,14 +4,11 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
   if ($_ENV['PANTHEON_ENVIRONMENT'] === 'dev') {
     $primary_domain = 'waf-protected-domain.com';
   }
-  else {
-    $primary_domain = $_SERVER['HTTP_HOST'];
-  }
 
   $requires_redirect = false;
   
   // Ensure the site is being served from the primary domain.
-  if ($_SERVER['HTTP_HOST'] != $primary_domain) {
+  if ($_SERVER['HTTP_HOST'] != $primary_domain ) {
     $requires_redirect = true;
   }
 
@@ -21,5 +18,4 @@ if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
     echo "WILL NOT REDIRECT";
   }
 }
-
 ?>
